d8dc62d5acc4eaa6733b369af49003bcf6e4f99d
Mirza Polat <46076246+mirzapolat@users.noreply.github.com>
2026-01-22 12:02:25 +0100

    Enhance type definitions for adm-zip module by adding getData method to entries

--- types/adm-zip.d.ts
+++ types/adm-zip.d.ts
@@ -2,7 +2,11 @@ declare module "adm-zip" {
   class AdmZip {
     constructor(zipFilePath?: string | Buffer);
     extractAllTo(targetPath: string, overwrite?: boolean): void;
-    getEntries(): Array<{ entryName: string; isDirectory: boolean }>;
+    getEntries(): Array<{
+      entryName: string;
+      isDirectory: boolean;
+      getData(): Buffer;
+    }>;
   }
 
   export default AdmZip;
