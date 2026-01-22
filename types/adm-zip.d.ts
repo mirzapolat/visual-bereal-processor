b6b48e5eb4d5999630b0829e843cfe35910726d9
Mirza Polat <46076246+mirzapolat@users.noreply.github.com>
2026-01-22 11:43:53 +0100

    Add type definitions for adm-zip module

---
+++ types/adm-zip.d.ts
@@ -0,0 +1,9 @@
+declare module "adm-zip" {
+  class AdmZip {
+    constructor(zipFilePath?: string | Buffer);
+    extractAllTo(targetPath: string, overwrite?: boolean): void;
+    getEntries(): Array<{ entryName: string }>;
+  }
+
+  export default AdmZip;
+}
