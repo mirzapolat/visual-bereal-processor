e361616c8886a7b77da4f494040e6b5423519f16
Mirza Polat <46076246+mirzapolat@users.noreply.github.com>
2026-01-22 12:04:05 +0100

    Add addLocalFolder and toBuffer methods to AdmZip class

--- types/adm-zip.d.ts
+++ types/adm-zip.d.ts
@@ -2,11 +2,13 @@ declare module "adm-zip" {
   class AdmZip {
     constructor(zipFilePath?: string | Buffer);
     extractAllTo(targetPath: string, overwrite?: boolean): void;
+    addLocalFolder(localPath: string, zipPath?: string): void;
     getEntries(): Array<{
       entryName: string;
       isDirectory: boolean;
       getData(): Buffer;
     }>;
+    toBuffer(): Buffer;
   }
 
   export default AdmZip;
